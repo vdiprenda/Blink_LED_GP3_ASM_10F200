6401bf9ef882416de14e8322e8d070cf3b1181de
Vincent DiPrenda <vdiprenda@gmail.com>
2022-09-08 10:01:30 -0400

    Initial Commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Blink_LED_GP3_ASM_10F200.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Blink_LED_GP3_ASM_10F200.X.production.hex
